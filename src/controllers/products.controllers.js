b258f8abe596d969f73a30d01d43460e1bd53fe6
Ivan Silva <silvaivan280@gmail.com>
2024-12-18 16:14:54 -0300

    agregue el controlador para obtener un producto mediante su id

---
+++ src/controllers/products.controllers.js
@@ -0,0 +1,46 @@
+import Product from "../models/products";
+
+export const getProducts = async (req, res) =>{
+    try {
+        const { category_id } = req.body;
+        const filter = category_id ? {category_id} : {};
+        const products = await Product.find(filter).populate('category_id', 'name');
+        res.status(200).json({
+            success: true,
+            products: products,
+            message: "Productos obtenidos exitosamenente"
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Error al obtener los productos',
+            error: error.message,
+        });
+    }
+}
+
+export const getProduct = async (req, res) =>{
+    try {
+        const product = await Product.findById(req.params.id);
+        if(!product){
+            return res.status(404).json({
+                success: false,
+                message: "Producto no encontrado",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            product,
+            message: "Productos obtenidos exitosamenente"
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Error al obtener el producto",
+            error: error.message
+        })
+    }
+}
+
