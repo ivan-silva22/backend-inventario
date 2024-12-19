ca58135732244dcffd209ff94c0161a77cb77d31
Ivan Silva <silvaivan280@gmail.com>
2024-12-19 15:32:15 -0300

    agregue la ruta de los movimientos del inventario

---
+++ src/routes/inventoryMovement.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { createInventoryMovement, deleteInventoryMovement, getInventoryMovement, getInventoryMovements } from "../controllers/inventoryMovementController";
+
+const router = Router();
+
+router.route("/inventoryMovements").post(createInventoryMovement).get(getInventoryMovements);
+router.route("/inventoryMovement/:id").get(getInventoryMovement).delete(deleteInventoryMovement);
