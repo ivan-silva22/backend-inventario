c2e6b4859059aef81a90b85485f2a32a7b8afc3a
Ivan Silva <silvaivan280@gmail.com>
2024-12-19 15:29:36 -0300

    agregue las rutas de los proveedores

---
+++ src/routes/supplier.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { createSupplier, deleteSupplier, getSupplier, getSuppliers, updatedSpplier } from "../controllers/supplier.controllers";
+
+const router = Router();
+
+router.route("/suppliers").post(createSupplier).get(getSuppliers);
+router.route("/supplier/:id").get(getSupplier).delete(deleteSupplier).put(updatedSpplier);
