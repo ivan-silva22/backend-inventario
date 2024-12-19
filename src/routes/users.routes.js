12bde3cc3d8ffb66dfa4b1ab1f491c30ed7c04e6
Ivan Silva <silvaivan280@gmail.com>
2024-12-19 15:26:41 -0300

    agregue las rutas de los usuarios

---
+++ src/routes/users.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { createUser, deleteUser, getUser, getUsers, updatedUser } from "../controllers/users.controllers";
+
+const router = Router();
+
+router.route("/users").post(createUser).get(getUsers);
+router.route("/user/:id").get(getUser).delete(deleteUser).put(updatedUser);
